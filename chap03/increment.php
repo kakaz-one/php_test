855ba87ab5b554eb4c5f5e10ddcef8f2ffafc8ae
kakaz-one <bousomusume2410836@gmail.com>
2025-09-03 18:28:34 +0900

    コメントを追加

--- chap03/increment.php
+++ chap03/increment.php
@@ -1,16 +1,16 @@
 <?php
 $x = 10;
 $y = ++$x;
-print $x;
-print $y;
+print $x; //結果：11
+print $y; //結果：11
 
 $i = 10;
 $j = $i++;
-print $i;
-print $j;
+print $i; //結果：11
+print $j; //結果：10
 
 $k = 10;
 $k++;
-print $k;
+print $k; //結果：11
 
 ?>
