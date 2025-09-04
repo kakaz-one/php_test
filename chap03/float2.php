6b0bbfc08080d9fcfca469fac1285a2e864b9b93
kakaz-one <bousomusume2410836@gmail.com>
2025-09-05 00:23:10 +0900

    BCMath拡張をインストール

--- chap03/float2.php
+++ chap03/float2.php
@@ -1,8 +1,5 @@
 <?php
-// 十分な精度を指定
-$add = bcadd('0.1', '0.7', 2);
+$add = bcadd('0.1', '0.7', 1);
 $mul = bcmul($add, '10', 1);
-print floor((float)$mul);
-
-// または通常の算術演算を使用
-$result = floor((0.1 + 0.7) * 10);
+print floor($mul); //結果：8 またエラーメッセージ（PHP Warning:  Module "bcmath" is already loaded in Unknown on line）が表示されているが、結果は8になる
+?>
